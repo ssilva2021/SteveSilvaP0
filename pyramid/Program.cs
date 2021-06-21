f5e5e9d4e9b651796d3c8f73b9b34ecc660a355a
ssilva2021 <stephen.silva@raveture.net>
2021-06-21 14:04:47 -0400

    hacker challenge

---
+++ pyramid/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for (int x=0; x< n; x++)
+        {
+            String spacePad = new String(' ',n-x);
+            String xPad = new String('X',x+1);
+            Console.WriteLine(spacePad + xPad);         
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
